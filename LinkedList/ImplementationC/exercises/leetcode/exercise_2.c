27c7fd256f8d08db461871da25d2188f2d5dee06
Physanto <hackmanu6@gmail.com>
2025-07-20 07:49:56 -0500

    refactor: se termino de implementar las funciones controladoras por ahora.

--- LinkedList/ImplementationC/exercises/leetcode/exercise_2.c
+++ LinkedList/ImplementationC/exercises/leetcode/exercise_2.c
@@ -8,15 +8,14 @@ struct ListNode {
 };
 
 /*
-Se le proporcionan dos listas enlazadas no vacías que representan dos enteros no negativos. Los dígitos se almacenan en orden inverso y cada uno de sus nodos contiene un solo dígito. Sume los dos números y devuelva la suma como una lista enlazada.
+Se le proporcionan dos listas enlazadas no vacías que representan dos enteros no negativos. 
+Los dígitos se almacenan en orden inverso y cada uno de sus nodos contiene un solo dígito. 
+Sume los dos números y devuelva la suma como una lista enlazada.
 
 Puedes asumir que los dos números no contienen ningún cero inicial, excepto el número 0 mismo.
-
-
 */
-int size = 0;
 
-void add_last(struct ListNode **list_head, int element, int *size);
+void add_last(struct ListNode **list_head, struct ListNode **tail, int element, int *size);
 
 void print(struct ListNode *list){ 
 
@@ -31,58 +30,56 @@ void print(struct ListNode *list){
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) { 
 
-    struct ListNode *current_l1 = l1;
-    struct ListNode *current_l2 = l2;
-
     struct ListNode *list;
+    struct ListNode *tail;
+
+    int number_l1 = l1->val;
+    int number_l2 = l2->val;
     int size = 0;
-
-    int number_l1 = current_l1->val;
-    int number_l2 = current_l2->val;
-
     int carry = 0;
     int add = 0; 
 
-    while(current_l1 != NULL || current_l2 != NULL){ 
+    while(l1 != NULL || l2 != NULL){ 
         
-        if(current_l1 == NULL) { number_l1 = 0;}
-        if(current_l2 == NULL) { number_l2 = 0;}
+        if(l1 == NULL) { number_l1 = 0;}
+        if(l2 == NULL) { number_l2 = 0;}
 
         add = number_l1 + number_l2;
 
+        struct ListNode *new_node = malloc(sizeof(struct ListNode));
+
         if(add >= 10){   
             add -= 10;
-            add_last(&list, (add+carry),&size);
+            add_last(&list, &tail,(add+carry),&size);
             carry = 1;
         }
         else {
             if((add+carry) == 10) {
-                add_last(&list, (0), &size);
+                add_last(&list, &tail, (0), &size);
                 carry = 1;
             }
             else {
-                add_last(&list, (add+carry), &size);
+                add_last(&list, &tail, (add+carry), &size);
                 carry = 0;
             }
         }
         
-        if(current_l1 != NULL) {
-            current_l1 = current_l1->next;
-            number_l1 = (current_l1 != NULL) ? current_l1->val : 0;
+        if(l1 != NULL) {
+            l1 = l1->next;
+            number_l1 = (l1 != NULL) ? l1->val : 0;
         }
-        if(current_l2 != NULL) {
-            current_l2 = current_l2->next;
-            number_l2 = (current_l2 != NULL) ? current_l2->val : 0;
+        if(l2 != NULL) {
+            l2 = l2->next;
+            number_l2 = (l2 != NULL) ? l2->val : 0;
         }
     }
 
-    if(carry == 1) add_last(&list, 1,&size);
+    if(carry == 1) add_last(&list,&tail, 1,&size);
 
     return list;
 }
-//l1 = [4,4,3], l2 = [6,7,4]
 
-void add_last(struct ListNode **list_head, int element, int *size){ 
+void add_last(struct ListNode **list_head, struct ListNode **tail, int element, int *size){ 
 
     struct ListNode *new_node = malloc(sizeof(struct ListNode));
     new_node->val = element;
@@ -91,18 +88,14 @@ void add_last(struct ListNode **list_head, int element, int *size){
 
     if(*size == 0){
         *list_head = new_node;
+        *tail = new_node;
         (*size)++;
         return;
     }
 
-    struct ListNode *current = *list_head;
-
-    while(current->next != NULL){
-        current = current->next;
-    }
-
-    current->next = new_node;
-    (*size)++;
+    (*tail)->next = new_node;
+    *tail = new_node;
+    size++;
 }
 
 void add_lastt(struct ListNode **list_head, int element){ 
