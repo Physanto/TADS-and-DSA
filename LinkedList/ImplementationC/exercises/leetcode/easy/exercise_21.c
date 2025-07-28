62e64d781f8d37ec0750a996c9af74ac60b77b3f
Physanto <hackmanu6@gmail.com>
2025-07-28 11:36:18 -0500

    nuevo ejercicio terminado

--- LinkedList/ImplementationC/exercises/leetcode/easy/exercise_21.c
+++ LinkedList/ImplementationC/exercises/leetcode/easy/exercise_21.c
@@ -5,6 +5,7 @@ struct ListNode {
     int val;
     struct ListNode *next;
 };
+
 /*Se le proporcionan los encabezados de dos listas enlazadas ordenadas list1 y list2.
 Fusionar las dos listas en una sola lista ordenada. Esta lista debe formarse
 uniendo los nodos de las dos primeras listas.
@@ -43,27 +44,27 @@ void print(struct ListNode *head){
     printf("NULL\n");
 }
 
-struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
-
-    struct ListNode *list = NULL;
-    int size = 0;
-
-    while(list1 != NULL || list2 != NULL){
-
-        if(list1->val == list2->val){
-            add_last(&list, list1->val, &size);
-            add_last(&list, list2->val, &size);
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    struct ListNode dummy;
+    struct ListNode* tail = &dummy;
+    dummy.next = NULL;
+    while(list1 && list2)
+    {
+        if(list1->val <= list2->val)
+        {
+            tail->next = list1;
+            list1 = list1->next;
         }
-        else if(list1->val < list2->val) add_last(&list, list1->val, &size);
-        else add_last(&list, list2->val, &size);
-
-        list1 = list1->next;
-        list2 = list2->next;
+        else
+        {
+            tail->next = list2;
+            list2 = list2->next;
+        }
+        tail = tail->next;
     }
-
-    return list;
+    tail->next = list1 ? list1 : list2;
+    return dummy.next;
 }
-
 int main(){
 
     struct ListNode *list1 = NULL;
@@ -71,14 +72,22 @@ int main(){
     int size = 0;
     int sizes = 0;
 
-    // add_last(&list1,2,&size);
-    // add_last(&list1,4,&size);
+    add_last(&list1,-10,&size);
+    add_last(&list1,-9,&size);
+    add_last(&list1,-6,&size);
+    add_last(&list1,-4,&size);
+    add_last(&list1,1,&size);
+    add_last(&list1,9,&size);
+    add_last(&list1,9,&size);
+    add_last(&list2,-5,&sizes);
+    add_last(&list2,-3,&sizes);
     add_last(&list2,0,&sizes);
-    // add_last(&list2,3,&sizes);
-    // add_last(&list2,4,&sizes);
-    //
-    // print(list1);
-    // print(list2);
+    add_last(&list2,7,&sizes);
+    add_last(&list2,8,&sizes);
+    add_last(&list2,8,&sizes);
+    print(list1);
+    print(list2);
+    printf("\n\n");
     print(mergeTwoLists(list1,list2));
     return 0;
 }
